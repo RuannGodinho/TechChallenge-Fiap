04fb3b72073c3fe4d6556b47e226329d742050d0
Ruann Godinho <ruann.godinho@fatec.sp.gov.br>
2026-05-06 22:33:25 -0300

    r todo

--- tests/unit/orderStatus.test.js
+++
@@ -1,5 +0,0 @@
-describe('Acompanhamento da Ordem de Serviço', () => {
-  //test.todo('Deve suportar status da OS: Recebida, Em diagnóstico, Aguardando aprovação, Em execução, Finalizada, Entregue');
-  //test.todo('Deve alterar automaticamente o status da OS conforme ações no sistema');
-  //test.todo('Deve permitir consulta do progresso da OS via API pelo cliente');
-});
